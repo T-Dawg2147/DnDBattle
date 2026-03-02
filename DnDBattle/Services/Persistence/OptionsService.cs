9d56f81338ec70df94881e5bae3874ef6974b2b2
Harry Calder-Ledgard <harryc@slingsby.com>
2026-03-02 11:37:50 +0000

    Revert "Backup 25/02/2026 12:57"
    This reverts commit 40f1352078ace359224028b7325636683c10b9d3.

--- DnDBattle/Services/Persistence/OptionsService.cs
+++ DnDBattle/Services/Persistence/OptionsService.cs
@@ -20,7 +20,6 @@ namespace DnDBattle.Services.Persistence
 {
     public static class OptionsService
     {
-
         private static readonly string AppFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "DnDBattle");
         private static readonly string ConfigFile = Path.Combine(AppFolder, "config.json");
 
