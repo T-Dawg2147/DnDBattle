c695623f3d992752952b6bf2d28a7f8037e8b68b
Harry Calder-Ledgard <harryc@slingsby.com>
2026-03-03 17:34:46 +0000

    Changes/Edits to be made to how tile maps are made and dealt with by the program.

--- DnDBattle/Services/TileService/TileLibraryService.cs
+++ DnDBattle/Services/TileService/TileLibraryService.cs
@@ -10,6 +10,7 @@ using System.Text;
 
 namespace DnDBattle.Services.TileService
 {
+    // The Library... Shh
     public class TileLibraryService
     {
         private static readonly Lazy<TileLibraryService> _instance =
